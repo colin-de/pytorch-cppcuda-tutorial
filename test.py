688c63607b519c61948afdb6616dc6b693425988
Weihang Li <weihang.li@tum.de>
2023-06-11 20:18:44 +0200

    update

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import torch
 import cppcuda_tutorial
 import time
 
-
 def trilinear_interpolation_py(feats, points):
     """
     Inputs:
@@ -48,24 +47,39 @@ class Trilinear_interpolation_cuda(torch.autograd.Function):
         dL_dfeats = cppcuda_tutorial.trilinear_interpolation_bw(dL_dfeat_interp.contiguous(), feats, points)
 
         return dL_dfeats, None
-
+    
 
 if __name__ == '__main__':
-    N = 65536; F = 256
+    print('cuda is available: ', torch.cuda.is_available())
+    print('cuda device count: ', torch.cuda.device_count())
+    print('cuda device name: ', torch.cuda.get_device_name(0))
+    print('cuda device capability: ', torch.cuda.get_device_capability(0))
+    print('cuda device properties: ', torch.cuda.get_device_properties(0))
+    print('cuda current device: ', torch.cuda.current_device())
+
+    N = 65535; F = 256
     rand = torch.rand(N, 8, F, device='cuda')
     feats = rand.clone().requires_grad_()
     feats2 = rand.clone().requires_grad_()
-    points = torch.rand(N, 3, device='cuda')*2-1
+
+    # feats = torch.rand(N, 8, F, device='cuda').requires_grad_()
+    # feats2 = torch.rand(N, 8, F, device='cuda').requires_grad_()
+    points = torch.rand(N, 3, device='cuda') * 2 - 1
 
     t = time.time()
-    out_cuda = Trilinear_interpolation_cuda.apply(feats2, points)
+    # out_cuda = cppcuda_tutorial.trilinear_interpolation_fw(feats, points)
+    out_cuda = Trilinear_interpolation_cuda.apply(feats, points)
+
     torch.cuda.synchronize()
-    print('   cuda fw time', time.time()-t, 's')
+    print('cuda fw time', time.time()-t, 's')
 
     t = time.time()
     out_py = trilinear_interpolation_py(feats, points)
     torch.cuda.synchronize()
     print('pytorch fw time', time.time()-t, 's')
+
+    print(out_cuda.shape)
+    print(out_py.shape)
 
     print('fw all close', torch.allclose(out_py, out_cuda))
 
@@ -73,12 +87,10 @@ if __name__ == '__main__':
     loss2 = out_cuda.sum()
     loss2.backward()
     torch.cuda.synchronize()
-    print('   cuda bw time', time.time()-t, 's')
+    print('cuda bw time', time.time()-t, 's')
 
     t = time.time()
     loss = out_py.sum()
     loss.backward()
     torch.cuda.synchronize()
     print('pytorch bw time', time.time()-t, 's')
-
-    print('bw all close', torch.allclose(feats.grad, feats2.grad))
